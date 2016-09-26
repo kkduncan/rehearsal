ce67d97d5897cef73984816e7781997166bcb2f7
kkduncan <kesterduncan@Kesters-iMac.local>
2016-09-25 22:54:15 -0400

    Updated license preamble in headers

--- include/core/core.hpp
+++ include/core/core.hpp
@@ -1,3 +1,24 @@
+/**
+ * trial
+ *
+ * This file is part of "trial."
+ *
+ *  trial is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * trial is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * \file core.hpp
+ * \author KDuncan
+ */
 #pragma once
 #ifndef KD_TRIAL_CORE_HPP_
 #define KD_TRIAL_CORE_HPP_
--- include/datastructures/linked_list.hpp
+++ include/datastructures/linked_list.hpp
@@ -1,6 +1,27 @@
+/**
+ * trial
+ *
+ * This file is part of "trial."
+ *
+ *  trial is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * trial is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * \file linked_list.hpp
+ * \author KDuncan
+ */
 #pragma once
-#ifndef TRIAL_LINKED_LIST_HPP_
-#define TRIAL_LINKED_LIST_HPP_
+#ifndef KD_TRIAL_LINKED_LIST_HPP_
+#define KD_TRIAL_LINKED_LIST_HPP_
 
 
-#endif /* TRIAL_LINKED_LIST_HPP_ */
+#endif /* KD_TRIAL_LINKED_LIST_HPP_ */
