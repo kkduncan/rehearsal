2998bd233d6f1a40b9f31e46567ee884458786d4
kkduncan <kkduncan@cse.usf.edu>
2016-10-04 11:29:26 -0400

    Update CMakeLists.txt for correct folder organization Updated license preamble in core.h and linked_list.h

--- include/core/core.h
+++ include/core/core.h
@@ -1,7 +1,5 @@
 /**
- * trial
- *
- *  trial is free software: you can redistribute it and/or modify
+ *  This is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
@@ -12,7 +10,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this.  If not, see <http://www.gnu.org/licenses/>.
  *
  * \file core.h
  * \author KDuncan
